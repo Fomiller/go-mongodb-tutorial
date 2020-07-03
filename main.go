30a6b37bfc5d13e27dec376b504499cab2d41401
Forrest <Forrestmillerj@gmail.com>
2020-07-02 21:04:03 -0500

    insert single and multiple document working

--- main.go
+++ main.go
@@ -35,6 +35,32 @@ func main() {
 	// Log message if successful
 	fmt.Println("Connected to MongoDB!")
 
+	// create collection trainer
+	collection := client.Database("go-mongo-tut").Collection("trainers")
+
+	// create trainers to add into database
+	ash := Trainer{"Ash", 10, "Pallet Town"}
+	misty := Trainer{"Misty", 10, "Cerulean City"}
+	brock := Trainer{"Brock", 15, "Pewter City"}
+
+	insertResult, err := collection.InsertOne(context.TODO(), ash)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// successful insert
+	fmt.Println("Inserted a single document: ", insertResult.InsertedID)
+
+	// create slice of trainers to insert at once
+	trainers := []interface{}{misty, brock}
+
+	insertManyResult, err := collection.InsertMany(context.TODO(), trainers)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("inserted multiple documents: ", insertManyResult.InsertedIDs)
+
+	// Route Handlers
 	http.HandleFunc("/", API.IndexHandler)
 	http.Handle("/favicon.ico", http.NotFoundHandler())
 	http.ListenAndServe(":8080", nil)
