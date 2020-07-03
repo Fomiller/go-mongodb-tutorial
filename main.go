3415f1f1e8f71bed48071b3f45d731e9c1b3e19c
Forrest <Forrestmillerj@gmail.com>
2020-07-02 20:38:30 -0500

    connected to mongodb

--- main.go
+++ main.go
@@ -1,12 +1,40 @@
 package main
 
 import (
+	"context"
+	"fmt"
+	"log"
 	"net/http"
 
 	"github.com/fomiller/go-mongodb-tutorial/API"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
+type Trainer struct {
+	Name string
+	Age  int
+	City string
+}
+
 func main() {
+	// Set client options
+	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
+
+	// connect to MongoDB
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Check the connection
+	err = client.Ping(context.TODO(), nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// Log message if successful
+	fmt.Println("Connected to MongoDB!")
+
 	http.HandleFunc("/", API.IndexHandler)
 	http.Handle("/favicon.ico", http.NotFoundHandler())
 	http.ListenAndServe(":8080", nil)
