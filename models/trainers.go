9ef9e959f65934f2ef080e18c68b680ad7d4eb32
Forrest <Forrestmillerj@gmail.com>
2020-07-04 15:13:16 -0500

    edits

--- models/trainers.go
+++ models/trainers.go
@@ -1,7 +1,7 @@
 package models
 
 type Trainer struct {
-	Name string
-	Age  int
-	City string
+	Name string `json:"name,omitempty" bson:"name,omitempty"`
+	Age  int    `json:"age,omitempty" bson:"age,omitempty"`
+	City string `json:"city,omitempty" bson:"city,omitempty"`
 }
